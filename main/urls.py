a5d497115c65f83ac535631aac9c9cf79c53d230
otuhii <olegtyhii81079@gmail.com>
2024-11-21 19:57:25 +0200

    changed login and added signup

--- main/urls.py
+++ main/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path("", views.indexPage, name="indexPage"),
     path("home/", views.homepageView, name="homepageView"),
     path("login/", views.loginView, name="loginView"),
-    path("signup/", views.signupView, name="signupView"),
     path("help/", views.helpPage, name="helpPage"),
+    path("logout/", views.logout_view, name="logout_view"),
     #path('profile/<str:username>/', views.view_profile, name='view_profile'),
     #path("uploadPhoto/<str:username>/", views.uploadphoto, name="uploadPhoto"),
-    #path("logout/", views.logout_view, name="logout_view"),
+    
+    
+    
+    path("signup/", views.signupView, name="signupView"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
-
-
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm
 
 
 
 
 def indexPage(request):
     return render(request, 'index/index.html')
-
-
 
 
 def homepageView(request):
@@ -21,12 +19,18 @@ def homepageView(request):
 def helpPage(request):
     return render(request, "help.html")
 
+def logout_view(request):
+    logout(request)
+    return redirect("indexPage")
+
 
 #AUTHENTICATION
+@csrf_exempt    
 def loginView(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
+        #phoneNum = request.POST["phoneNum"]
         user = authenticate(request=request, username=username, password=password)
         if user != None:
             login(request, user)
@@ -37,7 +41,17 @@ def loginView(request):
     return render(request, 'login/login.html')
 
 def signupView(request):
-    pass
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect("loginView") 
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup/signup.html', {
+        'form': form
+        })
 
 
 
